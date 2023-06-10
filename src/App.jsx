bc8073a637eeec6460c904baf5631c4d36009537
andry359 <a.revtov96@mail.ru>
2023-06-10 10:44:37 +0300

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import PrintPage from './pages/PrintPage';
-// import ArchitecturePage from './pages/ArchitecturePage';
-// import ExpositionTourPage from './pages/ExpositionTourPage';
+import ArchitecturePage from './pages/ArchitecturePage';
+import ExpositionTourPage from './pages/ExpositionTourPage';
 
 function App() {
 
   return (
     <div className="App">
-      <PrintPage />
+      <Routes>
+        <Route element={< PrintPage />} path={'/print'} key={'1'} />
+        <Route element={< ArchitecturePage />} path={'/architecture'} key={'2'} />
+        <Route element={< ExpositionTourPage />} path={'/map'} key={'3'} />
+      </Routes>
+      {/* <PrintPage /> */}
     </div>
   );
 }
